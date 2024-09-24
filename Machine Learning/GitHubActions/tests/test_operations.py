05c433ccbf0d2cfddb85c1f7dd4206fa69fccaab
Sanket Zanwar <sanketzanwar1128@gmail.com>
2024-09-23 21:02:41 -0400

    GitActions

--- Machine Learning/GitHubActions/tests/test_operations.py
+++ Machine Learning/GitHubActions/tests/test_operations.py
@@ -1,15 +1,17 @@
-import sys
 import os
+import sys
 
 # Add the src directory to the system path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 from MathOperations import add, subtract
+
 
 def test_add():
     """Test the add function with some values."""
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
 
 def test_subtract():
     """Test the subtract function with some values."""
